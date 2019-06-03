e21910ea9735b290a8423f2bbff654648e3a79e2
cisspbru <32341115+cisspbru@users.noreply.github.com>
2019-06-04 00:26:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import tensorflow as tf
 from object_detection.utils import ops as utils_ops
 from utils import label_maputil
 from utils import visualiztion_utils as vis_util
+
+#model file path
+model_file = 'graph.pb'
+detection_graph = tf.Graph()
