db9caf422e6bcf3f10091777fb4bfbbfb372e051
cisspbru <32341115+cisspbru@users.noreply.github.com>
2019-06-04 00:21:24 +0200

    create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from object_detection.utils import ops as utils_ops
+from utils import label_maputil
+from utils import visualiztion_utils as vis_util
