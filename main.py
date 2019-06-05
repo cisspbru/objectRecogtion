80f2142bb24e64534ef6ce897f180e82f791b8ae
cisspbru <32341115+cisspbru@users.noreply.github.com>
2019-06-05 22:46:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#import necessary libraries
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
@@ -7,5 +8,18 @@ from utils import label_maputil
 from utils import visualiztion_utils as vis_util
 
 #model file path
-model_file = 'graph.pb'
-detection_graph = tf.Graph()
+model_file = '/frozen_inference_graph.pb'
+detection_graph = tf.Graph() 
+with detection_graph.as_default():
+  od_grapg_def = tf.GraphDef()
+  with tf.gfile.GFile(model_file_name, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+    
+ 
+label_map_file_name = 'models/research/object_detection/data/mscoco_label_map.pbtxt'
+label_map = label_map_util.load_labelmap(label_map_file_name)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=90, use_display_name=True)
+
