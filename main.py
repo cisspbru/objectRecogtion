82673316ec4aead3bbf06d002fd78ca43828663e
cisspbru <32341115+cisspbru@users.noreply.github.com>
2019-10-15 13:07:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
+# IMPORTS
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
-import sys
 import tarfile
 import tensorflow as tf
-import zipfile
+import json
 
-from distutils.version import StrictVersion
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
+import sys
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
 
-# This is needed since the notebook is stored in the object_detection folder.
+
+# Object detection imports
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util    ### CWH: Add object_detection path
 
-#if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
- # raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
-
-
-# ## Env setup
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-
-from utils import label_map_util
-
-from utils import visualization_utils as vis_util
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_FROZEN_GRAPH` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
+# Model Preparation
 
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
@@ -47,57 +25,37 @@ MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt') ### CWH: Add object_detection path
+
+NUM_CLASSES = 100
 
 
-# ## Load a (frozen) Tensorflow model into memory.
-
-
-
+# Load a (frozen) Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-
-
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
 
-# ## Helper code
-
-
+# Helper code
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
-# # Detection
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
-
 with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
+  with tf.Session(graph=detection_graph, config = config) as sess:
     # Definite input and output Tensors for detection_graph
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
     # Each box represents a part of the image where a particular object was detected.
@@ -107,30 +65,50 @@ with detection_graph.as_default():
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    for image_path in TEST_IMAGE_PATHS:
-      image = Image.open(image_path)
-      # the array based representation of the image will be used later in order to prepare the
-      # result image with boxes and labels on it.
-      image_np = load_image_into_numpy_array(image)
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      # Actual detection.
-      (boxes, scores, classes, num) = sess.run(
-          [detection_boxes, detection_scores, detection_classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
- 
+
+# added to put object in JSON
+class Object(object):
+    #def __init__(self):
+
+    def toJSON(self):
+        return json.dumps(self.__dict__)
+
+def get_objects(image, threshold=0.5):
+    image_np = load_image_into_numpy_array(image)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Actual detection.
+    print('image loaded')
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+    print('run session ended')
+    classes = np.squeeze(classes).astype(np.int32)
+    scores = np.squeeze(scores)
+    boxes = np.squeeze(boxes)
+
+    obj_above_thresh = sum(n > threshold for n in scores)
+    print("detected %s objects in image above a %s score" % (obj_above_thresh, threshold))
+
+    output = []
 
 
+    for c in range(0, len(classes)):
+        class_name = category_index[classes[c]]['name']
+        if scores[c] >= threshold:      # only return confidences equal or greater than the threshold
+            print(" object %s - score: %s, coordinates: %s" % (class_name, scores[c], boxes[c]))
 
-classes = np.squeeze(classes).astype(np.int32)
-scores = np.squeeze(scores)
-boxes = np.squeeze(boxes)
- 
-threshold = 0.50  #CWH: set a minimum score threshold of 50%
-obj_above_thresh = sum(n > threshold for n in scores)
-print("detected %s objects in %s above a %s score" % ( obj_above_thresh, image_path, threshold))
- 
-for c in range(0, len(classes)):
-  if scores[c] > threshold:
-      class_name = category_index[classes[c]]['name']
-      print(" object %s is a %s - score: %s, location: %s" % (c, class_name, scores[c], boxes[c]))
+            item = Object()
+            item.name = 'Object'
+            item.class_name = class_name
+            item.score = float(scores[c])
+            item.y = float(boxes[c][0])
+            item.x = float(boxes[c][1])
+            item.height = float(boxes[c][2])
+            item.width = float(boxes[c][3])
+            output.append(item.__dict__)
+
+   
+    outputJson = json.dumps(output)
+    
+    return outputJson
